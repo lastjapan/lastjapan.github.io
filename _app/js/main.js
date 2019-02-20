92c925a787785050dd7732fb8f8c19ffeb9349b5
Jacob Thompson <thampsen@gmail.com>
2019-02-20 11:19:02 +0000

    fix some issues with js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,15 +1,10 @@
 // Define Dependencies
 var detect = require('./lib/detect');
-var jquery = require('./lib/jquery');
 var draggable = require('./modules/draggable');
-var coverVid = require('./modules/coverVid');
 
 // // Detect if JavaScript is enabled
 detect();
-
-jquery();
 draggable();
-coverVid();
 //
 // // Print success message to console
 // console.log('<head> scripts loaded.')
--- _app/js/modules/draggable.js
+++ _app/js/modules/draggable.js
@@ -1,9 +1,8 @@
 module.exports = function () {
-  var $ = require('./../lib/jquery');
   require('./../lib/jquery-ui');
 
-  jQuery(document).ready(function($) {
-    jQuery('.i-draggable').draggable().css({
+  $(document).ready(function($) {
+    $('.i-draggable').draggable().css({
       "left": (Math.random() * 80 + "vw"),
       "top": (Math.random() * 20 + "vw"),
     })
